acde224c9d481b00864e5ce6e0e12a352b9cbf5c
Jeremiah Bates <jermilive@gmail.com>
2024-01-04 19:26:29 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const date = new Date();
+
+function clickForDate() {
+  document.getElementById('onClick').innerHTML = date;
+}
+// let btn = (document.getElementById('btn', clickForDate).innerHTML = date);
+
+document.getElementById('displayDate').innerHTML = date;
